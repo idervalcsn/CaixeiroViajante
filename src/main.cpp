831343dce20107d4d22bfaa6560124685040e75d
rocinante02 <idervalsama@gmail.com>
2019-07-05 18:21:36 -0300

    movimento swap

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,36 @@
 
 using namespace std;
 
-struct CustoInsercao{
+struct CustoInsercao {
     int noInserido;
     int arestaRemovida;
     double custo;
 };
 
-double ** matrizAdj; // matriz de adjacencia
+double **matrizAdj; // matriz de adjacencia
 int dimension; // quantidade total de vertices
 
-bool    comparaCusto(CustoInsercao,CustoInsercao);
-int getCusto(vector<int>);
-void remover(vector<int> &v, int);
+bool comparaCusto(CustoInsercao, CustoInsercao); // Funcao de comparacao do sort
+vector<int> constructSolution();                   //constroi uma solucao
+vector<CustoInsercao>
+criaRestrita(double, vector<CustoInsercao> &); //cria uma lista restrita com os "alpha" melhores custos
+
+double deltaSwap(vector<int> &, int, int);     //calcula o delta de determinado movimento swap
+void movSwap(vector<int> &, int, int);            //executa um movimento swap
+
+double getCusto(vector<int> &);                      //custo total da solucao
+vector<CustoInsercao>
+listaDeCustos(vector<int> &, vector<int> &);   //cria uma lista de custos baseada nos candidatos restantes
 void printData();
-vector<int> constructSolution();
-vector<CustoInsercao> criaRestrita(double, vector<CustoInsercao>);
-vector<CustoInsercao> listaDeCustos(vector<int>,vector<int>);
+
+void remover(vector<int> &, int);                 //remover item atraves do valor
 
 
-int main(int argc, char** argv) {
+
+
+
+
+int main(int argc, char **argv) {
 
 
     srand(time(NULL));
@@ -34,52 +45,60 @@ int main(int argc, char** argv) {
     printData();
     vector<int> solution = constructSolution();
 
-    for(int i = 0; i <= dimension;i++){
-        cout << solution[i] << endl;
+    for (int i = 0; i <= dimension; i++) {
+        cout << solution[i] << "\t";
 
     }
-    cout <<  "Custo: " << getCusto(solution) << endl;
+    cout << endl << "Custo: " << getCusto(solution) << endl;
+    movSwap(solution, 2, 7);
+
+    for (int i = 0; i <= dimension; i++) {
+        cout << solution[i] << "\t";
+
+    }
+    cout << endl << "Custo: " << getCusto(solution) << endl;
+
+
 
     return 0;
 
-    
 
 }
 
-bool    comparaCusto(CustoInsercao x, CustoInsercao y){
+bool comparaCusto(CustoInsercao x, CustoInsercao y) {
     return x.custo < y.custo;
 }
 
 
-vector<int> constructSolution(){
+vector<int> constructSolution() {
 
 
-    vector<int> solucao = {1,1};  //Solucao comeca na cidade 1
+    vector<int> solucao = {1, 1};  //Solucao comeca na cidade 1
     vector<int> candidatos;
     int tamanhoSubtour = 4;
     //double custoTotal = matrizAdj[1][1];
 
-    for(int i = 2; i <= dimension; i++ ){ //Populando a lista de candidatos
+    for (int i = 2; i <= dimension; i++) { //Populando a lista de candidatos
         int atual = i;
         candidatos.push_back(atual);
     }
 
-    for(int i = 0; i < tamanhoSubtour; i++ ){ //Gerando subtour inicial
+    for (int i = 0; i < tamanhoSubtour; i++) { //Gerando subtour inicial
 
-        int j =  rand() % candidatos.size() ;
-        solucao.insert(solucao.begin() + 1, candidatos[j] );
+        int j = rand() % candidatos.size();
+        solucao.insert(solucao.begin() + 1, candidatos[j]);
         candidatos.erase(candidatos.begin() + j);
     }
 
-    while(!candidatos.empty()){
+    while (!candidatos.empty()) {
         vector<CustoInsercao> custos = listaDeCustos(solucao, candidatos);
-        sort(custos.begin(), custos.end(),comparaCusto);    //sorta baseado no custo de cada insercao
+        sort(custos.begin(), custos.end(), comparaCusto);    //sorta baseado no custo de cada insercao
         vector<CustoInsercao> listaRestrita = criaRestrita(0.5, custos);
-        int indiceAleatorio  =  rand() % listaRestrita.size();
+        int indiceAleatorio = rand() % listaRestrita.size();
         CustoInsercao escolhido = listaRestrita[indiceAleatorio];   //cidade escolhida, onde, e o custo
 
-        solucao.insert(solucao.begin() + (escolhido.arestaRemovida + 1),escolhido.noInserido);
-        remover(candidatos,escolhido.noInserido);
+        solucao.insert(solucao.begin() + (escolhido.arestaRemovida + 1), escolhido.noInserido);
+        remover(candidatos, escolhido.noInserido);
 
     }
 
@@ -88,13 +107,14 @@ vector<int> constructSolution(){
 
 }
 
-vector<CustoInsercao> listaDeCustos(vector<int> lista, vector<int> candidatos){
+vector<CustoInsercao> listaDeCustos(vector<int> &lista, vector<int> &candidatos) {
 
-    vector<CustoInsercao> custo((lista.size() - 1) * candidatos.size());
+    vector<CustoInsercao> custo((lista.size() - 1) *
+                                candidatos.size()); //cria uma lista de custos com o tamanho sendo: quantidade de arestas * candidatos restantes
 
-    for(int i = 0, j = 1, l = 0; i < lista.size() - 1; i++,j++){
-        for(auto k: candidatos){
-            custo[l].custo = matrizAdj[ lista[i] ][ k ] + matrizAdj[ lista[j] ][k] - matrizAdj[ lista[i] ][ lista[j ]];
+    for (int i = 0, j = 1, l = 0; i < lista.size() - 1; i++, j++) {
+        for (auto k: candidatos) {
+            custo[l].custo = matrizAdj[lista[i]][k] + matrizAdj[lista[j]][k] - matrizAdj[lista[i]][lista[j]];
             custo[l].noInserido = k;
             custo[l].arestaRemovida = i;
             l++;
@@ -104,16 +124,16 @@ vector<CustoInsercao> listaDeCustos(vector<int> lista, vector<int> candidatos){
     return custo;
 }
 
-vector<CustoInsercao> criaRestrita(double alpha, vector<CustoInsercao> lista){
+vector<CustoInsercao> criaRestrita(double alpha, vector<CustoInsercao> &lista) {
     int size = lista.size();
     double quantos = size * alpha; //quantidade de membros vai estar na lista restrita, baseado na pcentagem
     vector<CustoInsercao> restrita;
 
-    if(quantos < 1){    //quando quantos < 1, nao existe lista restrita a ser criada.
+    if (quantos < 1) {    //quando quantos < 1, nao existe lista restrita a ser criada.
         return lista;
     }
 
-    for(int i = 0; i < quantos; i++){
+    for (int i = 0; i < quantos; i++) {
         restrita.push_back(lista[i]);
     }
 
@@ -121,24 +141,43 @@ vector<CustoInsercao> criaRestrita(double alpha, vector<CustoInsercao> lista){
 
 }
 
-void remover(vector<int> &v, int item){   //remove um item de um vetor por valor
-    v.erase(remove(v.begin(),v.end(),item),v.end());
+void remover(vector<int> &v, int item) {   //remove um item de um vetor por valor
+    v.erase(remove(v.begin(), v.end(), item), v.end());
 }
 
-int getCusto(vector<int> solucao){
+double getCusto(vector<int> &solucao) {
     double custo = 0.0;
-    for(int i = 0, j = 1; i < solucao.size() - 1; i++,j++){
+    for (int i = 0, j = 1; i < solucao.size() - 1; i++, j++) {
         custo += matrizAdj[solucao[i]][solucao[j]];
     }
     return custo;
 }
 
-void printData() {
-  cout << "dimension: " << dimension << endl;
-  for (size_t i = 1; i <= dimension; i++) {
-    for (size_t j = 1; j <= dimension; j++) {
-      cout << matrizAdj[i][j] << " ";
+void movSwap(vector<int> &v, int no1, int no2){
+    int temp = v[no1];
+    v[no1] = v[no2];
+    v[no2] = temp;
+
+}
+
+double deltaSwap(vector<int> &v, int no1, int no2){
+    double** m = matrizAdj; //diminui o tamanho da declaraçao ali embaixo
+    double delta = 0;
+    if(no1 == no2) return delta;
+    else {
+        delta = m[v[no1]][v[no2 - 1]] + m[v[no1]][v[no2 + 1]] + m[v[no2]][v[no1 - 1]] + m[v[no2]][v[no1 + 1]]
+                - m[v[no1]][v[no1 - 1]] + m[v[no1]][v[no1 + 1]] + m[v[no2]][v[no2 - 1]] + m[v[no2]][v[no2 + 1]];
     }
-    cout << endl;
-  }
+
+    return delta;
+}
+
+void printData() {
+    cout << "dimension: " << dimension << endl;
+    for (size_t i = 1; i <= dimension; i++) {
+        for (size_t j = 1; j <= dimension; j++) {
+            cout << matrizAdj[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
